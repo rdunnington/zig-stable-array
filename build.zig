309e5f687958fd45893a8f68ebd246493dd385a7
Isaac Van Doren <isaacjvandoren@gmail.com>
2025-08-23 14:34:21 -0500

    upgrade to zig 15.1

--- build.zig
+++ build.zig
@@ -4,15 +4,13 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("zig-stable-array", .{
-        .root_source_file = b.path("stable_array.zig"),
-    });
+    _ = b.addModule("zig-stable-array", .{ .root_source_file = b.path("stable_array.zig") });
 
-    const lib_unit_tests = b.addTest(.{
+    const lib_unit_tests = b.addTest(.{ .root_module = b.createModule(.{
         .root_source_file = b.path("stable_array.zig"),
         .target = target,
         .optimize = optimize,
-    });
+    }) });
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
